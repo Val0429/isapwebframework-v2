ea0759467ee71eadce4e3b502795d97f74c4591a
Morris's Macbook Pro <Morris.Hsu@isapsolution.com>
2019-08-20 11:25:10 +0800

    append i18n in table date

--- core/i18n/en-US.ts
+++ core/i18n/en-US.ts
@@ -7,13 +7,13 @@ export default class LangObject {
     wb_PreviousPage = 'Prev';
     wb_Search = 'Search';
     wb_Browse = 'Browse';
-    wb_NextStep = "Next";
-    wb_PreviousStep = "Previous";
+    wb_NextStep = 'Next';
+    wb_PreviousStep = 'Previous';
     wb_Done = 'Done';
 
     wb_Login = 'Login';
-    wb_Username = "Username";
-    wb_Password = "Password";
+    wb_Username = 'Username';
+    wb_Password = 'Password';
 
     wb_Add = 'Add';
     wb_Delete = 'Delete';
@@ -38,24 +38,72 @@ export default class LangObject {
     mb_FormMultipleClickToAdd = 'Click to Add';
 
     mb_VersionLimitation = 'Version Limitation';
-    mb_VersionNotEnough = "ServerFramework version should be greater than v{version}.";
-    mb_PermissionDenied = "Permission Denied";
-    mb_PermissionDeniedLong = "Your Permission Cannot Access This Area.";
-    mb_ContentInvalid = "Content Invalid";
+    mb_VersionNotEnough = 'ServerFramework version should be greater than v{version}.';
+    mb_PermissionDenied = 'Permission Denied';
+    mb_PermissionDeniedLong = 'Your Permission Cannot Access This Area.';
+    mb_ContentInvalid = 'Content Invalid';
 
     mb_ShowEntries = 'Show {0} Entries';
     mb_ShowCurrentEntries = 'Showing {curStart} to {curEnd} of {total} entries';
     mb_PleaseSelect = 'Please Select';
     mb_ConfirmDelete = 'Are you sure to delete?';
 
-    mb_0 = "Request Failed";
-    mb_0_message = "No response from server. Generally happened because of ip / port error or server down.";
-    mb_400 = "Bad Request";
-    mb_400_message = "<b>Error Code {0}</b> - Server cannot process request due to client error. Check your parameters sent with request.";
-    mb_401 = "Unauthorized";
-    mb_401_message = "<b>Error Code {0}</b> - Authentication is required or has failed.";
-    mb_404 = "Not Found";
-    mb_404_message = "<b>Error Code {0}</b> - This resource cannot be accessed.";
-    mb_500 = "Internal Server Error";
-    mb_500_message = "<b>Error Code {0}</b> - Unknown server error.";
+    mb_0 = 'Request Failed';
+    mb_0_message = 'No response from server. Generally happened because of ip / port error or server down.';
+    mb_400 = 'Bad Request';
+    mb_400_message = '<b>Error Code {0}</b> - Server cannot process request due to client error. Check your parameters sent with request.';
+    mb_401 = 'Unauthorized';
+    mb_401_message = '<b>Error Code {0}</b> - Authentication is required or has failed.';
+    mb_404 = 'Not Found';
+    mb_404_message = '<b>Error Code {0}</b> - This resource cannot be accessed.';
+    mb_500 = 'Internal Server Error';
+    mb_500_message = '<b>Error Code {0}</b> - Unknown server error.';
+
+    // date time
+    mb_DateTime_ShortWeekDay0 = 'Sun';
+    mb_DateTime_ShortWeekDay1 = 'Mon';
+    mb_DateTime_ShortWeekDay2 = 'Tue';
+    mb_DateTime_ShortWeekDay3 = 'Wed';
+    mb_DateTime_ShortWeekDay4 = 'Thu';
+    mb_DateTime_ShortWeekDay5 = 'Fri';
+    mb_DateTime_ShortWeekDay6 = 'Sat';
+
+    mb_DateTime_NormalWeekDay0 = 'Sunday';
+    mb_DateTime_NormalWeekDay1 = 'Monday';
+    mb_DateTime_NormalWeekDay2 = 'Tuesday';
+    mb_DateTime_NormalWeekDay3 = 'Wednesday';
+    mb_DateTime_NormalWeekDay4 = 'Thursday';
+    mb_DateTime_NormalWeekDay5 = 'Friday';
+    mb_DateTime_NormalWeekDay6 = 'Saturday';
+
+    mb_DateTime_ShortMonth01 = 'Jan';
+    mb_DateTime_ShortMonth02 = 'Feb';
+    mb_DateTime_ShortMonth03 = 'Mar';
+    mb_DateTime_ShortMonth04 = 'Apr';
+    mb_DateTime_ShortMonth05 = 'May';
+    mb_DateTime_ShortMonth06 = 'Jun';
+    mb_DateTime_ShortMonth07 = 'Jul';
+    mb_DateTime_ShortMonth08 = 'Aug';
+    mb_DateTime_ShortMonth09 = 'Sep';
+    mb_DateTime_ShortMonth10 = 'Oct';
+    mb_DateTime_ShortMonth11 = 'Nov';
+    mb_DateTime_ShortMonth12 = 'Dec';
+
+    mb_DateTime_NormalMonth01 = 'January';
+    mb_DateTime_NormalMonth02 = 'February';
+    mb_DateTime_NormalMonth03 = 'March';
+    mb_DateTime_NormalMonth04 = 'April';
+    mb_DateTime_NormalMonth05 = 'May';
+    mb_DateTime_NormalMonth06 = 'June';
+    mb_DateTime_NormalMonth07 = 'July';
+    mb_DateTime_NormalMonth08 = 'August';
+    mb_DateTime_NormalMonth09 = 'September';
+    mb_DateTime_NormalMonth10 = 'October';
+    mb_DateTime_NormalMonth11 = 'November';
+    mb_DateTime_NormalMonth12 = 'December';
+
+    mb_DateTime_LowerAM = 'am';
+    mb_DateTime_LowerPM = 'pm';
+    mb_DateTime_UpperAM = 'AM';
+    mb_DateTime_UpperPM = 'PM';
 }
--- core/i18n/ja-JP.ts
+++ core/i18n/ja-JP.ts
@@ -8,13 +8,13 @@ export default class LangObject implements IBaseLang {
     wb_PreviousPage = '前へ';
     wb_Search = '検索する';
     wb_Browse = 'ブラウズ';
-    wb_NextStep = "次へ";
-    wb_PreviousStep = "前へ";
+    wb_NextStep = '次へ';
+    wb_PreviousStep = '前へ';
     wb_Done = '完了';
 
     wb_Login = 'ログイン';
-    wb_Username = "ユーザー";
-    wb_Password = "パスワード";
+    wb_Username = 'ユーザー';
+    wb_Password = 'パスワード';
 
     wb_Add = '追加';
     wb_Delete = '削除';
@@ -27,7 +27,7 @@ export default class LangObject implements IBaseLang {
 
     wb_Submit = '入力';
     wb_Reset = 'リセット';
-    
+
     wb_Yes = 'はい';
     wb_No = 'いいえ';
 
@@ -39,24 +39,72 @@ export default class LangObject implements IBaseLang {
     mb_FormMultipleClickToAdd = 'クリックしてデータを追加';
 
     mb_VersionLimitation = 'バージョン制限';
-    mb_VersionNotEnough = "ServerFrameworkのバージョンは v{version} より大きくなければなりません。";
-    mb_PermissionDenied = "アクセス拒否";
-    mb_PermissionDeniedLong = "このエリアにアクセスできない。";
-    mb_ContentInvalid = "コンテンツ無効";
+    mb_VersionNotEnough = 'ServerFrameworkのバージョンは v{version} より大きくなければなりません。';
+    mb_PermissionDenied = 'アクセス拒否';
+    mb_PermissionDeniedLong = 'このエリアにアクセスできない。';
+    mb_ContentInvalid = 'コンテンツ無効';
 
     mb_ShowCurrentEntries = '{total} 件中，{curStart} 件 - {curEnd} 件';
     mb_ShowEntries = '{0} ペンデータを表示';
     mb_PleaseSelect = '選択してください';
     mb_ConfirmDelete = '本当に削除しますか？';
 
-    mb_0 = "リクエストが失敗しました";
-    mb_0_message = "サーバーから応答がありません。 一般的にはIP /ポートエラーまたはサーバーのダウンが原因で発生しました。";
-    mb_400 = "リクエストが不正である";
-    mb_400_message = "<b>エラーコード {0}</b> - クライアントエラーのため、サーバーは要求を処理できません。 リクエストとともに送信されたパラメータを確認してください。";
-    mb_401 = "認証が必要である";
-    mb_401_message = "<b>エラーコード {0}</b> - 認証が必要か、または失敗しました。";
-    mb_404 = "未検出";
-    mb_404_message = "<b>エラーコード {0}</b> - このリソースにアクセスできません。";
-    mb_500 = "サーバ内部エラー";
-    mb_500_message = "<b>エラーコード {0}</b> - 不明なサーバーエラーです。";
+    mb_0 = 'リクエストが失敗しました';
+    mb_0_message = 'サーバーから応答がありません。 一般的にはIP /ポートエラーまたはサーバーのダウンが原因で発生しました。';
+    mb_400 = 'リクエストが不正である';
+    mb_400_message = '<b>エラーコード {0}</b> - クライアントエラーのため、サーバーは要求を処理できません。 リクエストとともに送信されたパラメータを確認してください。';
+    mb_401 = '認証が必要である';
+    mb_401_message = '<b>エラーコード {0}</b> - 認証が必要か、または失敗しました。';
+    mb_404 = '未検出';
+    mb_404_message = '<b>エラーコード {0}</b> - このリソースにアクセスできません。';
+    mb_500 = 'サーバ内部エラー';
+    mb_500_message = '<b>エラーコード {0}</b> - 不明なサーバーエラーです。';
+
+    // date time
+    mb_DateTime_ShortWeekDay0 = '日曜';
+    mb_DateTime_ShortWeekDay1 = '月曜';
+    mb_DateTime_ShortWeekDay2 = '火曜';
+    mb_DateTime_ShortWeekDay3 = '水曜';
+    mb_DateTime_ShortWeekDay4 = '木曜';
+    mb_DateTime_ShortWeekDay5 = '金曜';
+    mb_DateTime_ShortWeekDay6 = '土曜';
+
+    mb_DateTime_NormalWeekDay0 = '日曜日';
+    mb_DateTime_NormalWeekDay1 = '月曜日';
+    mb_DateTime_NormalWeekDay2 = '火曜日';
+    mb_DateTime_NormalWeekDay3 = '水曜日';
+    mb_DateTime_NormalWeekDay4 = '木曜日';
+    mb_DateTime_NormalWeekDay5 = '金曜日';
+    mb_DateTime_NormalWeekDay6 = '土曜日';
+
+    mb_DateTime_ShortMonth01 = '一月';
+    mb_DateTime_ShortMonth02 = '二月';
+    mb_DateTime_ShortMonth03 = '三月';
+    mb_DateTime_ShortMonth04 = '四月';
+    mb_DateTime_ShortMonth05 = '五月';
+    mb_DateTime_ShortMonth06 = '六月';
+    mb_DateTime_ShortMonth07 = '七月';
+    mb_DateTime_ShortMonth08 = '八月';
+    mb_DateTime_ShortMonth09 = '九月';
+    mb_DateTime_ShortMonth10 = '十月';
+    mb_DateTime_ShortMonth11 = '十一月';
+    mb_DateTime_ShortMonth12 = '十二月';
+
+    mb_DateTime_NormalMonth01 = 'いちがつ';
+    mb_DateTime_NormalMonth02 = 'にがつ';
+    mb_DateTime_NormalMonth03 = 'さんがつ';
+    mb_DateTime_NormalMonth04 = 'しがつ';
+    mb_DateTime_NormalMonth05 = 'ごがつ';
+    mb_DateTime_NormalMonth06 = 'ろくがつ';
+    mb_DateTime_NormalMonth07 = 'しちがつ';
+    mb_DateTime_NormalMonth08 = 'はちがつ';
+    mb_DateTime_NormalMonth09 = 'くがつ';
+    mb_DateTime_NormalMonth10 = 'じゅうがつ';
+    mb_DateTime_NormalMonth11 = 'じゅういちがつ';
+    mb_DateTime_NormalMonth12 = 'じゅうにがつ';
+
+    mb_DateTime_LowerAM = '朝';
+    mb_DateTime_LowerPM = '午後';
+    mb_DateTime_UpperAM = '朝';
+    mb_DateTime_UpperPM = '午後';
 }
