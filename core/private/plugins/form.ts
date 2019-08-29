128d65b9693373d4c89b3f4b5e74f68030b1c6f3
Val <val.liu@isapsolution.com>
2019-08-29 10:17:55 +0800

    fix bug: $form can now perform freely

--- core/private/plugins/form.ts
+++ core/private/plugins/form.ts
@@ -19,16 +19,27 @@ export const FormPlugin = {
 
         Vue.mixin({
             methods: {
-                $form: function(this: Vue, ref: string, keys?: string): void {
+                $form: function(this: Vue, ref: string, keys?: string): any {
+                    var me: any = this;
+                    /// link to form
+                    setTimeout(() => {
+                        let watchRef = (me.$refs[ref] || {})._uid;
+                        if (!watchRef || me._watchRef == watchRef) return;
+                        me._watchRef = watchRef;
+                        /// recover the link
+                        this.$forceUpdate();
+                    }, 0);
+
+                    /// calculate result
                     let result = (<any>((this.$refs || {})[ref] || {})).result;
-                    if (result == null) return;
+                    if (result == null) return null;
                     while (result instanceof Vue) result = result.result;
-                    if (result == null) return;
+                    if (result == null) return null;
                     if (!keys) return result;
 
                     for (let key of keys.split(".")) {
                         result = result[key];
-                        if (result == null) return;
+                        if (result == null) return null;
                     }
                     return result;
                 }
