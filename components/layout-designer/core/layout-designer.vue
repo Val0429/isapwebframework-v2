50c7bb7613f8b70c0e2178b066005e500bae7f2d
Val <val.liu@isapsolution.com>
2019-11-18 13:21:15 +0800

    layout designer half way

---
+++ components/layout-designer/core/layout-designer.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="editor">
+        <editor-menu-bar :editor="editor" v-slot="{ commands, isActive, focused }">
+            <div class="menubar is-hidden"
+                :class="{ 'is-focused': focused }">
+                
+                <iv-toolbox-custom icon="fa-bold" @click="commands.bold" />
+
+            </div>
+        </editor-menu-bar>
+
+        <editor-content class="editor__content" :editor="editor" />
+    </div>
+</template>
+
+
+<script lang="ts" src="./layout-designer.vue.ts" />
+
+
+<style lang="scss" scoped>
+.menubar {
+    border: 1px solid #BBB;
+    border-left: 0;
+    border-right: 0;
+    margin-bottom: 1em;
+    padding: 3px 10px;
+
+    // &.is-hidden {
+    //     visibility: hidden;
+    //     opacity: 0;
+    // }
+
+    &.is-focused {
+        visibility: visible;
+        opacity: 1;
+        transition: visibility .2s, opacity .2s;
+    }
+}
+</style>
---
+++ components/layout-designer/core/layout-designer.vue.ts
@@ -0,0 +1,68 @@
+/*
+ * Created on Tue Nov 18 2019
+ * Author: Val Liu
+ * Copyright (c) 2019, iSAP Solution
+ */
+
+import { Vue, Component, Prop, Model, Watch } from "vue-property-decorator";
+import { generateIcon } from "@/../core/utilities";
+
+// import { schema } from 'prosemirror-schema-basic';
+// import { DOMParser, Schema } from 'prosemirror-model';
+// import { EditorState, Plugin } from "prosemirror-state";
+// import { EditorView } from "prosemirror-view";
+// import { undo, redo, history } from "prosemirror-history";
+// import { keymap } from "prosemirror-keymap";
+// import { baseKeymap } from "prosemirror-commands";
+// import { exampleSetup } from "prosemirror-example-setup";
+
+import { Editor, EditorContent, EditorMenuBar } from 'tiptap';
+import {
+  Blockquote,
+  CodeBlock,
+  HardBreak,
+  Heading,
+  HorizontalRule,
+  OrderedList,
+  BulletList,
+  ListItem,
+  TodoItem,
+  TodoList,
+  Bold,
+  Code,
+  Italic,
+  Link,
+  Strike,
+  Underline,
+  History,
+} from 'tiptap-extensions';
+
+@Component({
+    components: { EditorContent, EditorMenuBar }
+})
+export class LayoutDesigner extends Vue {
+    @Prop({
+        type: String, required: false
+    })
+    value: string;
+
+    /// private helper
+    @Watch("value", { immediate: true })
+    onValueChanged(newval: string) {
+        console.log('content change!', newval);
+        this.editor.setContent(newval, true);
+    }
+
+    /// init
+    private editor = new Editor({
+        extensions: [
+            new Bold()
+        ]
+    });
+
+    /// recycle
+    private beforeDestroy() {
+        this.editor.destroy();
+    }
+}
+export default LayoutDesigner;
