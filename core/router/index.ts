de77f1716385fd62ad0b222577982a00807eb689
Val <val.liu@isapsolution.com>
2019-12-06 16:32:37 +0800

    router now export route

--- core/router/index.ts
+++ core/router/index.ts
@@ -141,6 +141,7 @@ function resolveRoute(config: IRegisterRouterInput) {
 }
 
 const sjDefaultRoute: BehaviorSubject<any> = new BehaviorSubject(null);
+export const obDefaultRoute = sjDefaultRoute.asObservable();
 export default new Promise<Router>( (resolve) => {
     setTimeout( () => {
         let router = new Router({
