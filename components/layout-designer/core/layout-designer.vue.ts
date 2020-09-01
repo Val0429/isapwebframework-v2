f055fc937b7dd678d8f07f5a657b3361d2763c09
Tina.Lin <Tina.Lin@isapsolution.com>
2020-09-01 12:00:02 +0800

    package | update

--- components/layout-designer/core/layout-designer.vue.ts
+++ components/layout-designer/core/layout-designer.vue.ts
@@ -4,43 +4,24 @@
  * Copyright (c) 2019, iSAP Solution
  */
 
-import { Vue, Component, Prop, Model, Watch } from "vue-property-decorator";
-import { generateIcon } from "@/../core/utilities";
+import { Vue, Component, Prop, Model, Watch } from 'vue-property-decorator';
+import { generateIcon } from '@/../core/utilities';
 
 import { Editor, EditorContent, EditorMenuBar } from 'tiptap';
-import {
-  Blockquote,
-  CodeBlock,
-  HardBreak,
-  Heading,
-  HorizontalRule,
-  OrderedList,
-  BulletList,
-  ListItem,
-  TodoItem,
-  TodoList,
-  Bold,
-  Code,
-  Italic,
-  Link,
-  Strike,
-  Underline,
-  History,
-  Placeholder
-} from 'tiptap-extensions';
 import { TextDesigner, Font } from './../elements';
 
 @Component({
-    components: { EditorContent, EditorMenuBar }
+    components: { EditorContent, EditorMenuBar },
 })
 export class LayoutDesigner extends Vue {
     @Prop({
-        type: String, required: false
+        type: String,
+        required: false,
     })
     value: string;
 
     /// private helper
-    @Watch("value", { immediate: true })
+    @Watch('value', { immediate: true })
     onValueChanged(newval: string) {
         console.log('content change!', newval);
         this.editor.setContent(newval, true);
@@ -48,25 +29,14 @@ export class LayoutDesigner extends Vue {
 
     /// init
     private editor = new Editor({
-        extensions: [
-            new TextDesigner(),
-            new Bold(),
-            new Heading({ labels: [1,2,3] }),
-            new Font()
-            // new Placeholder({
-            //     emptyNodeClass: "is-empty",
-            //     emptyNodeText: "Write something...",
-            //     showOnlyWhenEditable: true,
-            // })
-        ],
-        onUpdate: ({getJSON, getHTML}) => {
+        onUpdate: ({ getJSON, getHTML }) => {
             console.log('updated!', getHTML());
         },
         editorProps: {
             handlePaste: (view, event, slice) => {
                 console.log('paste!', event, slice);
-            }
-        }
+            },
+        },
     });
 
     /// recycle
